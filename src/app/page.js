5db09d9baba5439ba1fdb9f5f045cab02e22efaa
Nicholas Ng <nickng-1998@hotmail.com>
2024-06-06 01:19:53 +0800

    implement hit counter

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,8 @@
-import React from 'react';
+import React from "react"
 
-import {
-  readFile,
-  writeFile,
-} from '../helpers/file-helpers';
+import { readFile, writeFile } from "../helpers/file-helpers"
 
-const DATABASE_PATH = '/src/database.json';
+const DATABASE_PATH = "/src/database.json"
 
 /*
 `readFile` takes 1 argument:
@@ -24,12 +21,22 @@ writeFile(
 */
 
 function Home() {
+  let currentViewCount = 0
+  try {
+    const fileContent = JSON.parse(readFile(DATABASE_PATH))
+    currentViewCount = +fileContent?.hits
+  } catch (e) {}
+
+  const nextViewCount = currentViewCount + 1
+
+  writeFile(DATABASE_PATH, `{ "hits": "${nextViewCount}" }`)
+
   return (
     <main>
       <h1>Welcome!</h1>
-      <p>You are visitor number X.</p>
+      <p>You are visitor number {nextViewCount}.</p>
     </main>
-  );
+  )
 }
 
-export default Home;
+export default Home
